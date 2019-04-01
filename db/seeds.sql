fd7feb2e813f1704e83303fb0ed752dc8dce8f72
Pia Tello <PiaTello@MacBook-Pro.local>
2019-04-01 01:34:44 -0400

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('BacoNator', false);
+INSERT INTO burgers (name, devoured) VALUES ('West Coast', false);
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast Burger', false);
+
